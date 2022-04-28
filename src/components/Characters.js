b24f337af3c9d31decf94cd3d36fcf59eb8f4ff1
Kwame007 <clementnyankuadjei@gmail.com>
2022-04-28 22:54:12 +0000

    render new pagination component

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,58 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import CaharacterInput from "./CharacterInput";
-import Character from "./Character";
-import classes from "./Characters.module.css";
+import React from "react";
+import CharacterInput from "./CharacterInput";
+import Pagination from "./Pagination";
 
-const Characters = (props) => {
-  // character states
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [query, setQuery] = useState("");
-
-  // use effect
-  useEffect(() => {
-    axios
-      .get(`https://www.breakingbadapi.com/api/characters?name=${query}`)
-      .then((res) => {
-        setData(res.data);
-        setIsLoading(false);
-        setError(false);
-      })
-      .catch((err) => {
-        setError(true);
-        // setIsLoading(false);
-      });
-  }, [query]);
-
-  // lifting state
-  const getCharacterQuery = (query) => {
-    setQuery(query);
-  };
-
+const Characters = () => {
   return (
     <>
       {/* search input */}
-      <CaharacterInput
-        className={"container center"}
-        onChange={getCharacterQuery}
-      />
-      {/* error component] */}
-      {error && <h3>Error occured</h3>}
+      <CharacterInput className={"container center"} />
 
-      {/* loading component */}
-      {isLoading && <h2>Loading</h2>}
-
-      {/* character list */}
-      <div className={`${classes.cards} ${props.className}`}>
-        {!isLoading && data.length === 0 ? <h3>No character found</h3> : ""}
-
-        {!isLoading &&
-          data.map((character) => (
-            <Character data={character} key={character.char_id} />
-          ))}
-      </div>
+      {/* [pagination] */}
+      <Pagination className={"container"} pageLimit={5} dataLimit={12} />
     </>
   );
 };
