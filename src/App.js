d6e7539a25749b4717176933fb5bb3ba4f04b9f8
Kwame007 <clementnyankuadjei@gmail.com>
2022-04-28 23:00:33 +0000

    update characters component with new pagination component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "./index.css";
 import Header from "./UI/Header";
 import Characters from "./components/Characters";
-
 function App() {
   return (
     <>
+      {/* Breaking bad react rebuild 🔥 */}
       <Header className={"center"} />
       <Characters className={"container"} />
+
+      
     </>
   );
 }
