ba06624c8ec206e897956ed1d0c539f6039a3fe1
Kwame007 <clementnyankuadjei@gmail.com>
2022-04-29 23:54:46 +0000

    manage paginatedData state with useReducer hook

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,27 +4,55 @@ import classes from "./Pagination.module.css";
 import Character from "./Character";
 import { CharacterContext } from "../context/CharacterContextProvider";
 
+// reducer function
+const paginateDataReducer = (state, action) => {
+  console.log(state);
+  switch (action.type) {
+    case "DATA_CHANGE":
+      return { paginateData: action.paginateData };
+    case "SET_NEW_DATA":
+      return { paginateData: action.paginateData };
+    default:
+      return {
+        paginateData: null,
+      };
+  }
+};
+
 const Pagination = (props) => {
-  const { data, isLoading, error, pageLimit } = useContext(CharacterContext);
-  const [paginateData, setPaginatedData] = useState(null);
+  const { data, isLoading, error } = useContext(CharacterContext);
+
+  // const [paginateData, setPaginatedData] = useState(null);
   const [pages, setPages] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
 
   // manage states with useReducer
-  const [state, dispatch] = useReducer(() => {}, { data: "" });
+  const [state, dispatch] = useReducer(paginateDataReducer, {
+    paginateData: null,
+  });
 
   useEffect(() => {
     // check if data is valid
     if (data) {
       setPages(Math.round(data.length / props.dataLimit));
-      setPaginatedData(data);
+      // setPaginatedData(data);
+      dispatch({
+        type: "DATA_CHANGE",
+        paginateData: data,
+      });
 
       // get start and end index from original characters array {data}
       const startIndex = currentPage * props.dataLimit - props.dataLimit;
       const endIndex = currentPage + props.dataLimit - 1;
 
       // update {pagenatedData} with sliced array {which will contain the paginated array to be renderd}
-      setPaginatedData((prevData) => prevData.slice(startIndex, endIndex));
+      // setPaginatedData((prevData) => prevData.slice(startIndex, endIndex));
+      dispatch({
+        type: "SET_NEW_DATA",
+        paginateData: data.slice(startIndex, endIndex),
+      });
+
+      // console.log(data.slice(startIndex, endIndex));
     }
   }, [pages, data, props.dataLimit, currentPage]);
 
@@ -50,8 +78,8 @@ const Pagination = (props) => {
       {isLoading && <h2>Loading</h2>}
 
       <div className={`${classes.cards} ${props.className}`}>
-        {!isLoading && paginateData
-          ? paginateData.map((character) => (
+        {!isLoading && state.paginateData
+          ? state.paginateData.map((character) => (
               <Character data={character} key={character.char_id} />
             ))
           : ""}
